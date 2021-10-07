1bf61f729d074ffcaba5dfea1c545ba9d919483c
Adam Hinton <adam.d.hinton@gmail.com>
2021-10-07 17:57:17 -0400

    need to finish 6 and 7

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+  const object = {name: name, price: price, category: category,};
+  return object;
 }
+// console.log(createMenuItem('tacos', 8, 'Lunch'));
+//this passes
+
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -29,6 +34,11 @@ Test your createMenuItems function by doing the following:
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
 
+// console.log(createMenuItem("pizza", 5, "lunch"));
+// console.log(createMenuItem("salad", 8, "dinner"));
+// console.log(createMenuItem("coffee", 0.75, "breakfast"));
+
+//this passes
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -47,9 +57,21 @@ const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  
-}
+  discount: function(position){
+    if(position === 'teacher'){
+      return this.price * 0.75;
+    }
+    else if (position === 'student'){
+      return this.price * 0.75;
+    }
+    else {
+      return this.price * 0.90;
+    }
+  }
+};
 
+// console.log(burger.discount('teacher'));
+//all good here
 
 
 ///////////////Reviews (MVP)///////////////////
@@ -68,7 +90,9 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
+// console.log(reviews[5].feedback);
 
+//there's not actually a test associated with this
 
 
 
@@ -77,13 +101,16 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
+// reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
 
+// console.log(reviews);
+//no test associated with this
 
 
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Write a function that creates an object with name, rating, feedback, add the new review to the end of an array and returns the resulting array
+Write a function that creates an object with name, rating, feedback, adds the new review to the end of an array and returns the resulting array
  the addReview function below to do the following:
   1. Receive an array
   2. Receive the following object keys name, rating, feedback
@@ -91,9 +118,14 @@ Write a function that creates an object with name, rating, feedback, add the new
   4. should return the resulting array
 */
 
-function addReview(/*Your Code Here */){
-  /*Your Code Here */
-}
+function addReview(reviews, name, rating, feedback){
+  reviews.push({name, rating, feedback,});
+  return reviews;
+};
+// console.log(addReview(reviews, 'Daniela', 5, 'Beautiful atmosphere and wonderful vegan options!'));
+// This passes
+
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
@@ -106,12 +138,16 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
-}
+function getReviewByIndex(myArray, myIndex) {
+for (let i= 0; i < myArray.length; i++){
+  if (myIndex === i){
+return `${name} gave the restaurant a ${'rating'} star review, and their feedback was: ${'feedback'}`;
+  }
+  }
+};
 
+// console.log(getReviewByIndex(reviews, 0))
 
-  
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Write a function to get information about the most recent (last) review called `getLastReview`
@@ -125,9 +161,11 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
-} 
+function getLastReview(reviewsList) {
+return `${name} gave the restaurant a ${rating} star review, and their feedback was: ${feedback}`
+} ;
+
+// console.log(getLastReview)
 
 
 
